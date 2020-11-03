231138bce3f1ba805e8250dbd3d917c1686646fe
xiaocchen <xiaocchen@tencent.com>
2020-11-03 15:35:17 +0800

    基础环境搭建

---
+++ yongchuan/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# @Author  : jkl
+# @Email   : 189156395@qq.com
+# @Time: 2020/11/3 14:33
+# @File: __init__.py.py
---
+++ yongchuan/yongchuan/apps/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# @Author  : jkl
+# @Email   : 189156395@qq.com
+# @Time: 2020/11/3 14:32
+# @File: __init__.py.py
